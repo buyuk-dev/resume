cadbb9a08546dd2f3c3be5a3d4257cfdd8e81925
Michał Michalski <michal@buyuk-dev.com>
2024-01-27 22:29:46 +0100

    build script now creates MichalskiMichal.pdf;

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 # sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-extra-utils texlive-latex-extra
 pandoc -V colorlinks=true -V linkcolor=blue -V geometry:margin=1.5cm -o resume.pdf resume.md --wrap=preserve
+cp resume.pdf MichalskiMichal.pdf
